857fe6e0626db4720695f2c6d6c4e739c28037bf
jbouzy-laptop <joshbouzy@gmail.com>
2019-11-29 15:55:15 -0500

    Added image and timers

--- CountingValleys/CountingValleys.py
+++ CountingValleys/CountingValleys.py
@@ -1,3 +1,14 @@
+import os
+import sys
+import time
+
+from os import system, name   
+def clear(): 
+    if name == 'nt': 
+        _ = system('cls')   
+    else: 
+        _ = system('clear') 
+
 def countingValleys(steps):
     altitude = 0
     valleyCount = 0
@@ -16,8 +27,31 @@ def countingValleys(steps):
 
     return (valleyCount)
 
+print(r"""
+
+        _    .  ,   .           .
+    *  / \_ *  / \_      _  *        *   /\'__        *
+      /    \  /    \,   ((        .    _/  /  \  *'.
+ .   /\/\  /\/ :' __ \_  `          _^/  ^/    `--.
+    /    \/  \  _/  \-'\      *    /.' ^_   \_   .'\  *
+  /\  .-   `. \/     \ /==~=-=~=-=-;.  _/ \ -. `_/   \
+ /  `-.__ ^   / .-'.--\ =-=~_=-=~=^/  _ `--./ .-'  `-
+/jgs     `.  / /       `.~-^=-=~=^=.-'      '-._ `._
+
+Challenge: Counting Valleys
+
+ASCII Artist: Joan Stark @ https://www.asciiart.eu/
+      
+  """)
+
+time.sleep(5)
+clear() 
+time.sleep(2)
+
 steps = input("Input the string of steps taken with 'D' for down and 'U' for up. Leave no space in between: ")
 
 result = countingValleys(steps)
 
-print("There were a total of " + str(result) + " valleys.\n")
+print("\nThere were a total of " + str(result) + " valley(s).\n")
+
+time.sleep(10)
